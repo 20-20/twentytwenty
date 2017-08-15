cceafc219790e156007691cb63c74a901a330987
Darryn Wu <darryn8wu@gmail.com>
2017-08-14 21:06:30 -0400

    initial commit

---
+++ extension/SidebarHTML.js
@@ -0,0 +1,12 @@
+const sidebar =
+	`<div class='annotate-sidebar'>
+		<nav class="panel">
+			<p class="panel-heading annotate-header">
+				Comments
+			</p>
+			<div class='annotate-list'>
+			</div>
+			<input type=submit class='annotate-save' value='Save'>
+			<input class='annotate-text-entry' placeholder='Type notes here!'>
+		</nav>
+	</div>`;
