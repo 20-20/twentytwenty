1d8558e48cabce4eafe49e68f7c1f74311a30a21
Andrew Atkinson <blind.sam@gmail.com>
2017-08-18 12:55:38 -0400

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const app = require('APP'), {env} = app
-
-module.exports = {
-  clientID: `917356370239-l124ctr60safa5t7egeh966qrreim6nf.apps.googleusercontent.com`,
-  clientSecret: `XbZ1tUCjK7yaQskh8a0cKHNf`,
-  callbackURL: `${app.baseUrl}/api/auth/login/google`
-}
