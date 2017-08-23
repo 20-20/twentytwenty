e17885c98cd94288213a909e3bb41fd86efccb16
andrew-atkinson <blind.sam@gmail.com>
2017-08-23 16:26:42 -0400

    fix: moved radar chart

--- app/components/SingleArticle.jsx
+++ app/components/SingleArticle.jsx
@@ -4,8 +4,9 @@ import { Navlink } from 'react-router-dom'
 import { fetchArticle } from '../reducers/singleArticle'
 import Comments from './Comments.jsx'
 import Radar from 'react-d3-radar'
+import RadarChart from './radarChart'
 
-class SingleArticle extends Component{
+class SingleArticle extends Component {
 
   componentDidMount() {
     console.log('componentProps', this.props)
@@ -13,7 +14,7 @@ class SingleArticle extends Component{
     this.props.fetchArticle(articleId)
   }
 
-  render(){
+  render() {
     const singleArticle = this.props.singleArticle
 
     return (
@@ -23,7 +24,7 @@ class SingleArticle extends Component{
           {
             <div className="columns">
               <div className="column is-two-thirds">
-                <h1 className="title is-1">{ singleArticle && singleArticle.title}</h1>
+                <h1 className="title is-1">{singleArticle && singleArticle.title}</h1>
                 <hr />
                 <h2 className="subtitle is-3">{singleArticle && singleArticle.publication}</h2>
                 <a href={singleArticle && singleArticle.url}>Link to Article</a>
@@ -37,51 +38,20 @@ class SingleArticle extends Component{
                 }
               </div>
               <Comments />
+              <div className="column">
+                <RadarChart singleArticle={this.props.singleArticle} />
+              </div>
+
             </div>
           }
         </div>
-        <Radar
-          width={500}
-          height={500}
-          padding={70}
-          domainMax={1}
-          highlighted={null}
-          onHover={(point) => {
-            if (point) {
-              console.log('hovered over a data point');
-            } else {
-              console.log('not over anything');
-            }
-          }}
-          data={{
-            variables: [
-              { key: 'joy', label: 'Joy' },
-              { key: 'anger', label: 'Anger' },
-              { key: 'disgust', label: 'Disgust' },
-              { key: 'sadness', label: 'Sadness' },
-              { key: 'fear', label: 'Fear' }
-            ],
-            sets: [
-              {
-                key: 'me',
-                label: 'My Scores',
-                values: {
-                  joy: `${singleArticle.joy}`,
-                  anger: `${singleArticle.anger}`,
-                  disgust: `${singleArticle.disgust}`,
-                  sadness: `${singleArticle.sadness}`,
-                  fear: `${singleArticle.fear}`,
-                },
-              }
-            ],
-          }}
-        />
+
       </div>
     )
   }
 }
 
-const mapStateToProps = function({ trending, comments, singleArticle }) {
+const mapStateToProps = function ({ trending, comments, singleArticle }) {
   return {
     trending,
     comments,
@@ -90,7 +60,7 @@ const mapStateToProps = function({ trending, comments, singleArticle }) {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchArticle: ( articleId ) => dispatch(fetchArticle( articleId ))
+  fetchArticle: (articleId) => dispatch(fetchArticle(articleId))
 })
 
 const singleArticleContainer = connect(mapStateToProps, mapDispatchToProps)(SingleArticle)
